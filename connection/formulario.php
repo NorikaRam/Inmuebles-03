d6707452af4cfb4f56b8b8bbc2a1aba5ee8e47f9
Norika <norisz.ramirez@gmail.com>
2023-06-06 19:23:59 -0600

    Inmuebles

---
+++ connection/formulario.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inmuebles</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+<header>
+        <nav>
+            <div class="navegacion">
+                <img src="images/logo.svg" alt="Logo">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+            </div>
+            <h1> Venta de casas y departamentos</h1>
+        </nav>
+    </header>
+    <section class="formulario">
+    <h4> Formulario Registro </h4>
+    <input class="controls" type="text" name ="nombres"id =" names" placeholder="Ingrese su nombre">
+    <input class="controls" type="text" name ="nombres"id =" names" placeholder="Ingrese su apellido">
+    <input class="controls " type="email" name ="nombres"id =" names" placeholder="Ingrese su correo">
+    <input class="controls" type="pasword" name ="nombres"id =" names" placeholder="Ingrese su contraseña">
+    
+    </section>
+    <p> Estoy de acuerdo con <a href="#">Terminos y condiciones </a></p>
+    <input class="botons"type="submit" value= "Registrar">
+    <p><a href="#"> ¿Ya tengo cuenta?</a></p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inmuebles</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="navegacion">
+                <img src="images/logo.svg" alt="Logo">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+            </div>
+            <h1> Venta de casas y departamentos</h1>
+        </nav>
+    </header>
+    <main>
+        <h1> Más de nosotros </h1>
+        <section class="iconos">
+            <div class="iconos-nosotros">
+                <div class="icono">
+                    <img src="images/icono1.svg" alt="Icono seguridad" loading="lazy">
+                    <h3>Seguridad</h3>
+                    <p>
+                        Estar seguro en el mundo en línea se vuelve cada vez más importante,
+                        ya que la presencia online va en crecimiento y es vital proteger los datos que 
+                        tienes en Internet. 
+                    </p>
+                </div>
+                <div class="icono">
+                    <img src="images/icono2.svg"alt="Icono precio"loading="lazy">
+                    <h3>Precio</h3>
+                    <p>
+                    Estar Buscando casas y no encontrar resultados, debido a que no conoces y 
+                    te las pueden dar más caras de los que realmente cuentan. 
+                    </p>
+                </div>
+                <div class="icono">
+                    <img src="images/icono3.svg"alt="Icono tiempo"loading="lazy">
+                    <h3>Tiempo</h3>
+                    <p>
+                        Estar seguro en el mundo en línea se vuelve cada vez más importante,
+                        ya que la presencia online va en crecimiento y es vital proteger los datos que 
+                        tienes en Internet. 
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="contenedor-casas">
+            <h2>Casas y departamentos en Venta</h2>
+            <div class="contenedor-anuncios">
+                <div class="anuncio-casa">
+                    <picture>
+                        <img src="images/anuncio1.jpg" alt="anuncios"loading="lazy" >
+                    </picture>
+                    <div class="contenido-anuncio">
+                        <h3>Casa frente al lago </h3>
+                        <p> Casa acogedora frente al lago con excelente vista </p>
+                        <p class="precio">4,500,000</p>
+                        <ul class="iconos-caracteristicas">
+                            <li>        
+                                <img src="images/icono_wc.svg" alt="Baños">
+                                <p>4</p>
+                            </li>
+                            <li>        
+                                <img src="images/icono_estacionamiento.svg" alt="Estacionamientos">
+                                <p>2</p>
+                            </li>
+                            <li>        
+                                <img src="images/icono_dormitorio.svg" alt="Dormitorios">
+                                <p>5</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <button>Ver Casa</button>
+                </div>
+                <div class="anuncio-casa">
+                    <picture>
+                        <img src="images/anuncio2.jpg" alt="anuncios"loading="lazy" >
+                    </picture>
+                    <div class="contenido-anuncio">
+                        <h3>Casa residencial amueblada </h3>
+                        <p> Casa acogedora en buen lugar para vivir </p>
+                        <p class="precio">3,000,000</p>
+                        <ul class="iconos-caracteristicas">
+                            <li>        
+                                <img src="images/icono_wc.svg" alt="Baños">
+                                <p>3</p>
+                            </li>
+                            <li>        
+                                <img src="images/icono_estacionamiento.svg" alt="Estacionamientos">
+                                <p>4</p>
+                            </li>
+                            <li>        
+                                <img src="images/icono_dormitorio.svg" alt="Dormitorios">
+                                <p>3</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <button>Ver Casa</button>
+                </div>
+                <div class="anuncio-casa">
+                    <picture>
+                        <img src="images/anuncio3.jpg" alt="anuncios"loading="lazy" >
+                    </picture>
+                    <div class="contenido-anuncio">
+                        <h3>Casa con alberca </h3>
+                        <p> Excelente casa, para vivir  </p>
+                        <p class="precio">4,500,000</p>
+                        <ul class="iconos-caracteristicas">
+                            <li>        
+                                <img src="images/icono_wc.svg" alt="Baños">
+                                <p>3</p>
+                            </li>
+                            <li>        
+                                <img src="images/icono_estacionamiento.svg" alt="Estacionamientos">
+                                <p>3</p>
+                            </li>
+                            <li>        
+                                <img src="images/icono_dormitorio.svg" alt="Dormitorio">
+                                <p>3</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <button>Ver Casa</button>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="navegacion">
+                <a href="nosotros.html">Nosotros</a>
+                <a href="anuncios.html">Anuncios</a>
+                <a href="blog.html">Blog</a>
+                <a href="contacto.html">Contacto</a>
+            </div>
+            <p class="copyrigth">Derechos Reservados &copy; Norika Isabel - 2023 </p>
+        </footer>
+    </main>
+</body>
+</html>
